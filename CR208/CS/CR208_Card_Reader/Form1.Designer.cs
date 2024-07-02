e604cce711d876b7c017262a04ffe964b07e1c18
vohungvi <vohungvi@vohungvi.com>
2024-07-02 21:54:09 +0700

    + add project

---
+++ CR208/CS/CR208_Card_Reader/Form1.Designer.cs
@@ -0,0 +1,398 @@
+﻿namespace CR208_Card_Reader
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.radioButton4 = new System.Windows.Forms.RadioButton();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.groupBox4 = new System.Windows.Forms.GroupBox();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.btn_led = new System.Windows.Forms.Button();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.btn_beep = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txt_cardNumber = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.btn_read = new System.Windows.Forms.Button();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.txt_received = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txt_port = new System.Windows.Forms.TextBox();
+            this.btn_connect = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.lbl_message = new System.Windows.Forms.ToolStripStatusLabel();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.chk_enableBeep = new System.Windows.Forms.CheckBox();
+            this.chk_turnOnLED = new System.Windows.Forms.CheckBox();
+            this.groupBox4.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.groupBox3.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // radioButton4
+            // 
+            this.radioButton4.AutoSize = true;
+            this.radioButton4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.radioButton4.Location = new System.Drawing.Point(294, 19);
+            this.radioButton4.Name = "radioButton4";
+            this.radioButton4.Size = new System.Drawing.Size(137, 24);
+            this.radioButton4.TabIndex = 16;
+            this.radioButton4.Text = "Red and Green";
+            this.radioButton4.UseVisualStyleBackColor = true;
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.radioButton3.Location = new System.Drawing.Point(203, 19);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(72, 24);
+            this.radioButton3.TabIndex = 15;
+            this.radioButton3.Text = "Green";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Checked = true;
+            this.radioButton1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.radioButton1.Location = new System.Drawing.Point(21, 19);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(49, 24);
+            this.radioButton1.TabIndex = 13;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "Off";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // groupBox4
+            // 
+            this.groupBox4.Controls.Add(this.radioButton4);
+            this.groupBox4.Controls.Add(this.radioButton3);
+            this.groupBox4.Controls.Add(this.radioButton2);
+            this.groupBox4.Controls.Add(this.radioButton1);
+            this.groupBox4.Controls.Add(this.btn_led);
+            this.groupBox4.Dock = System.Windows.Forms.DockStyle.Top;
+            this.groupBox4.Location = new System.Drawing.Point(0, 264);
+            this.groupBox4.Name = "groupBox4";
+            this.groupBox4.Size = new System.Drawing.Size(800, 64);
+            this.groupBox4.TabIndex = 21;
+            this.groupBox4.TabStop = false;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.radioButton2.Location = new System.Drawing.Point(112, 19);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(57, 24);
+            this.radioButton2.TabIndex = 14;
+            this.radioButton2.Text = "Red";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // btn_led
+            // 
+            this.btn_led.Enabled = false;
+            this.btn_led.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.btn_led.Location = new System.Drawing.Point(509, 19);
+            this.btn_led.Name = "btn_led";
+            this.btn_led.Size = new System.Drawing.Size(88, 34);
+            this.btn_led.TabIndex = 12;
+            this.btn_led.Text = "LED";
+            this.btn_led.UseVisualStyleBackColor = true;
+            this.btn_led.Click += new System.EventHandler(this.btn_led_Click);
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.numericUpDown1.Location = new System.Drawing.Point(139, 23);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            999,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Minimum = new decimal(new int[] {
+            10,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(120, 26);
+            this.numericUpDown1.TabIndex = 13;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            100,
+            0,
+            0,
+            0});
+            // 
+            // btn_beep
+            // 
+            this.btn_beep.Enabled = false;
+            this.btn_beep.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.btn_beep.Location = new System.Drawing.Point(509, 16);
+            this.btn_beep.Name = "btn_beep";
+            this.btn_beep.Size = new System.Drawing.Size(88, 34);
+            this.btn_beep.TabIndex = 11;
+            this.btn_beep.Text = "Beep";
+            this.btn_beep.UseVisualStyleBackColor = true;
+            this.btn_beep.Click += new System.EventHandler(this.btn_beep_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.label2.Location = new System.Drawing.Point(26, 51);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(101, 20);
+            this.label2.TabIndex = 11;
+            this.label2.Text = "Card number";
+            // 
+            // txt_cardNumber
+            // 
+            this.txt_cardNumber.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.txt_cardNumber.Location = new System.Drawing.Point(139, 48);
+            this.txt_cardNumber.Name = "txt_cardNumber";
+            this.txt_cardNumber.Size = new System.Drawing.Size(352, 26);
+            this.txt_cardNumber.TabIndex = 10;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.label1.Location = new System.Drawing.Point(27, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(72, 20);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "Receved";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.label3.Location = new System.Drawing.Point(27, 26);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(88, 20);
+            this.label3.TabIndex = 14;
+            this.label3.Text = "Beep delay";
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.label3);
+            this.groupBox3.Controls.Add(this.numericUpDown1);
+            this.groupBox3.Controls.Add(this.btn_beep);
+            this.groupBox3.Dock = System.Windows.Forms.DockStyle.Top;
+            this.groupBox3.Location = new System.Drawing.Point(0, 188);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(800, 76);
+            this.groupBox3.TabIndex = 20;
+            this.groupBox3.TabStop = false;
+            // 
+            // btn_read
+            // 
+            this.btn_read.Enabled = false;
+            this.btn_read.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.btn_read.Location = new System.Drawing.Point(509, 28);
+            this.btn_read.Name = "btn_read";
+            this.btn_read.Size = new System.Drawing.Size(88, 34);
+            this.btn_read.TabIndex = 8;
+            this.btn_read.Text = "Read";
+            this.btn_read.UseVisualStyleBackColor = true;
+            this.btn_read.Click += new System.EventHandler(this.btn_read_Click);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.chk_turnOnLED);
+            this.groupBox2.Controls.Add(this.chk_enableBeep);
+            this.groupBox2.Controls.Add(this.label2);
+            this.groupBox2.Controls.Add(this.txt_cardNumber);
+            this.groupBox2.Controls.Add(this.label1);
+            this.groupBox2.Controls.Add(this.btn_read);
+            this.groupBox2.Controls.Add(this.txt_received);
+            this.groupBox2.Dock = System.Windows.Forms.DockStyle.Top;
+            this.groupBox2.Location = new System.Drawing.Point(0, 65);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(800, 123);
+            this.groupBox2.TabIndex = 19;
+            this.groupBox2.TabStop = false;
+            // 
+            // txt_received
+            // 
+            this.txt_received.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.txt_received.Location = new System.Drawing.Point(139, 19);
+            this.txt_received.Name = "txt_received";
+            this.txt_received.Size = new System.Drawing.Size(352, 26);
+            this.txt_received.TabIndex = 7;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.label4.Location = new System.Drawing.Point(27, 28);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(38, 20);
+            this.label4.TabIndex = 15;
+            this.label4.Text = "Port";
+            // 
+            // txt_port
+            // 
+            this.txt_port.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.txt_port.Location = new System.Drawing.Point(139, 25);
+            this.txt_port.Name = "txt_port";
+            this.txt_port.Size = new System.Drawing.Size(352, 26);
+            this.txt_port.TabIndex = 14;
+            this.txt_port.Text = "COM4";
+            // 
+            // btn_connect
+            // 
+            this.btn_connect.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.btn_connect.Location = new System.Drawing.Point(509, 19);
+            this.btn_connect.Name = "btn_connect";
+            this.btn_connect.Size = new System.Drawing.Size(88, 34);
+            this.btn_connect.TabIndex = 13;
+            this.btn_connect.Text = "Connect";
+            this.btn_connect.UseVisualStyleBackColor = true;
+            this.btn_connect.Click += new System.EventHandler(this.btn_connect_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.txt_port);
+            this.groupBox1.Controls.Add(this.btn_connect);
+            this.groupBox1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.groupBox1.Location = new System.Drawing.Point(0, 0);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(800, 65);
+            this.groupBox1.TabIndex = 18;
+            this.groupBox1.TabStop = false;
+            // 
+            // lbl_message
+            // 
+            this.lbl_message.Font = new System.Drawing.Font("Segoe UI", 12F);
+            this.lbl_message.Name = "lbl_message";
+            this.lbl_message.Size = new System.Drawing.Size(157, 21);
+            this.lbl_message.Text = "toolStripStatusLabel1";
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.lbl_message});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 424);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(800, 26);
+            this.statusStrip1.TabIndex = 17;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // chk_enableBeep
+            // 
+            this.chk_enableBeep.AutoSize = true;
+            this.chk_enableBeep.Checked = true;
+            this.chk_enableBeep.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.chk_enableBeep.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.chk_enableBeep.Location = new System.Drawing.Point(139, 86);
+            this.chk_enableBeep.Name = "chk_enableBeep";
+            this.chk_enableBeep.Size = new System.Drawing.Size(124, 24);
+            this.chk_enableBeep.TabIndex = 12;
+            this.chk_enableBeep.Text = "Beep on read";
+            this.chk_enableBeep.UseVisualStyleBackColor = true;
+            // 
+            // chk_turnOnLED
+            // 
+            this.chk_turnOnLED.AutoSize = true;
+            this.chk_turnOnLED.Checked = true;
+            this.chk_turnOnLED.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.chk_turnOnLED.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.chk_turnOnLED.Location = new System.Drawing.Point(294, 86);
+            this.chk_turnOnLED.Name = "chk_turnOnLED";
+            this.chk_turnOnLED.Size = new System.Drawing.Size(167, 24);
+            this.chk_turnOnLED.TabIndex = 13;
+            this.chk_turnOnLED.Text = "Turn on Green LED";
+            this.chk_turnOnLED.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.groupBox4);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.statusStrip1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosed);
+            this.groupBox4.ResumeLayout(false);
+            this.groupBox4.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RadioButton radioButton4;
+        private System.Windows.Forms.RadioButton radioButton3;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.GroupBox groupBox4;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.Button btn_led;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Button btn_beep;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txt_cardNumber;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.Button btn_read;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.TextBox txt_received;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txt_port;
+        private System.Windows.Forms.Button btn_connect;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.ToolStripStatusLabel lbl_message;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.CheckBox chk_enableBeep;
+        private System.Windows.Forms.CheckBox chk_turnOnLED;
+    }
+}
+
---
+++ CR208/CS/CR208_Card_Reader/Form1.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using CR208Wrapper;
+
+namespace CR208_Card_Reader
+{
+    public partial class Form1 : Form
+    {
+        CR208 cardReader = new CR208();
+
+        //////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        //////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            cardReader.Disconnect();
+        }
+
+        //////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+        private void btn_connect_Click(object sender, EventArgs e)
+        {
+            bool connectSuccess = cardReader.Connect(txt_port.Text);
+            if (connectSuccess)
+            {
+                btn_read.Enabled = true;
+                btn_beep.Enabled = true;
+                btn_led.Enabled = true;
+
+                lbl_message.Text = "Connected";
+            }
+            else
+            {
+                btn_read.Enabled = false;
+                btn_beep.Enabled = false;
+                btn_led.Enabled = false;
+
+                lbl_message.Text = "Cannot connect";
+            }
+        }
+
+        //////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+        private void btn_read_Click(object sender, EventArgs e)
+        {
+            int delay = 0;
+            if (chk_enableBeep.Checked)
+                delay = (int)numericUpDown1.Value;
+
+
+            string cardNumber = cardReader.Read(delay, chk_turnOnLED.Checked);
+            txt_cardNumber.Text = cardNumber;
+
+            if (cardNumber == "")
+                lbl_message.Text = "Cannot read";
+            else
+                lbl_message.Text = "Read success";
+        }
+
+        //////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+        private void btn_beep_Click(object sender, EventArgs e)
+        {
+            int delay = (int)numericUpDown1.Value;
+            cardReader.Beep(delay);
+        }
+
+        //////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+        private void btn_led_Click(object sender, EventArgs e)
+        {
+            if (radioButton1.Checked)
+                cardReader.TurnLed(false, false);
+            else if (radioButton2.Checked)
+                cardReader.TurnLed(true, false);
+            else if (radioButton3.Checked)
+                cardReader.TurnLed(false, true);
+            else if (radioButton4.Checked)
+                cardReader.TurnLed(true, true);
+        }
+    }
+}
